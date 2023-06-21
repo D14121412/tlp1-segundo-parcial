ab78bedd64384b1b47d607e085fd99d249d796b1
Barboza Daira <daiyanela2008@gmail.com>
2023-06-21 14:58:04 -0300

    Agregado modelo de datos para las reservas

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,31 @@
-// TODO: Crear modelo de datos de Reserva
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database/connection");
+
+const Reserva = sequelize.define(
+  "Reserva",
+  {
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    apellido: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    codigoReserva: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    fechaReserva: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "reservas",
+    timestamps: true,
+  }
+);
+
+module.exports = Reserva;
