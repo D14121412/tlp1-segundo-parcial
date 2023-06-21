d4801ff8023559ef31c5b7fcbb70bf693ccd83fe
Barboza Daira <daiyanela2008@gmail.com>
2023-06-21 15:15:18 -0300

    Agregado archivo database.js para la conexión a la base de datos MySQL

---
+++ database.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const Sequelize = require("sequelize");
+const app = express();
+
+const sequelize = new Sequelize("reserva.db", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+const reservaModel = sequelize.define("reservas", {
+  id: { type: Sequelize.INTEGER, primaryKey: true },
+  apellido: Sequelize.STRING,
+  codigoReserva: Sequelize.STRING,
+  fechaReserva: Sequelize.DATE,
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conexión a la base de datos exitosa");
+  })
+  .catch((error) => {
+    console.log("Error de conexión " + error);
+  });
+
+usuarioModel
+  .findAll({
+    attributes: ["nombre", "apellido", "codigoReserva", "fechaReserva"],
+  })
+  .then((reservas) => {
+    const resultados = JSON.stringify(reservas);
+    console.log(resultados);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
